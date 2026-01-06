26b38f0f6b186af6269f044819a97f4e95896863
Emmanuel Mwizerwa <mwizerwaemmanuel7@gmail.com>
2026-01-06 05:18:17 +0300

    Update Image.js ONLY FIXED IMAGE

--- models/Image.js
+++ models/Image.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 
 const imageSchema = new mongoose.Schema({
-  imageUrl: { type: String, required: true },
+  imageUrl: { 
+    type: String, 
+    required: true,
+    default: 'https://huta-nine.vercel.app/Formular.JPG' // Set default image
+  },
   description: { type: String, required: true },
   link: { type: String, required: true },
   categories: [{ type: String, required: true }],
-  paragraph: { type: String, default: '' }, // <-- Added this line
+  paragraph: { type: String, default: '' },
   createdAt: { type: Date, default: Date.now }
 });
 
